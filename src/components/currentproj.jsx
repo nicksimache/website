27d6602d88c2cf54f97b2c0115e7fac1fb92058f
nicksima <nicksimache@gmail.com>
2024-05-21 14:51:41 -0700

    current project updated

--- src/components/currentproj.jsx
+++ src/components/currentproj.jsx
@@ -4,46 +4,73 @@ import '../App.scss';
 const CurrentProject = () => {
 
     const [hovered, setHovered] = useState(false);
-    const [hoverStart, setHoverStart] = useState(null);
-    const [hoverTime, setHoverTime] = useState(0);
+    const [showDiv, setShowDiv] = useState(false);  
 
-    let hoverClass;
+    const overlay_style = {
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: hovered ? 'rgb(225, 225, 225)' : 'rgb(191, 191, 191)',
+        background: hovered ? 'rgba(50, 50, 50, 0.5)' : 'rgba(0, 0, 0, 0.5)',
+        cursor: hovered ? 'pointer' : 'default',
+        fontFamily: 'NunitoSans, sans-serif',
+        fontFize: '40px',
+        boxSizing: 'border-box',
+        borderRadius: '10px',
+        pointerEvents: 'auto',
 
-    useEffect(() => {
-        let timer1;
-        let timer2;
+    }
 
-        if (hovered) {
-            setHoverStart(Date.now());
-
-            timer1 = setTimeout(() => {
-                hoverClass = "hover1";
-            }, 1000);
-
-            timer2 = setTimeout(() => {
-                hoverClass = "hover2";
-            }, 5000)
-
-        } else {
-            setHoverStart(null);
-            setHoverTime(0);
-            clearTimeout(timer1);
-            clearTimeout(timer2);
-        }
-
-        return (() => {
-            clearTimeout(timer1);
-            clearTimeout(timer2);
-        })
-    }, [hovered]);
+    const expandedDiv = {
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: showDiv ? '90vw' : '0',
+        height: showDiv ? '90vh' : '0',
+        backgroundColor: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        transition: 'width 0.2s ease-in-out, height 0.2s ease-in-out'
+    };
+    
+    const handleClick = () => {
+        setShowDiv(!showDiv);
+    };
 
     return (
         <div 
-            className={'CurrentProject ${hoverClass}'}
+            className='currentProject'
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
+            onClick={handleClick}
         >
-            <h1>h1</h1>
+            <div 
+                className='overlay'
+                style={overlay_style}
+            >
+                <h2>Currently Working On</h2>
+            </div>
+            <img 
+                src={'../../public/dungeongenerateexample.png'} 
+                alt="Current Project" 
+                className='project-image' 
+            />
+            {showDiv && 
+            <div
+                style={expandedDiv}
+                onClick={handleClick}
+            >
+                
+                
+            </div>}
         </div>
     );
 }
---
+++ src/components/currentprojexpanded.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import '../App.scss';
+
+const CurrentProjectExpanded = () => {
+
+}
+
+export default CurrentProjectExpanded;
